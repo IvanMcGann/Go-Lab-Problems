6deee6dc7e05905e9be12bc033ed360411501293
IVAN MC GANN - Student <G00340138@gmit.ie>
2017-10-06 18:51:09 +0100

    added comments for problem 3 FizzBuzz, will test again now

--- 03-Fizz_Buzz.go
+++ 03-Fizz_Buzz.go
@@ -1,6 +1,15 @@
+//adapted from https://gist.github.com/sergiotapia/7884903 
+
+//implements main method
 package main
 
+//import the format package
 import "fmt"
+
+//main method function
+//initially i is set to 1 to begin the for loop, if i is a modulus of 3 AND 5 eg 15 Fizzbuzz will be displayed to the screen
+//However if i is a modulus of 3 e.g. 9 only Fizz will be displayed to the console
+//Finally if i is only a modulus of 5 Buzz will be displayed to the user
 
 func main() {
     i := 1
