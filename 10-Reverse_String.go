891244c4eecd86178322649a916e9a2b9ac3b554
IVAN MC GANN - Student <G00340138@gmit.ie>
2017-10-06 19:21:48 +0100

    changed some code and added comments

--- 10-Reverse_String.go
+++ 10-Reverse_String.go
@@ -1,29 +1,26 @@
+//adapted from https://coderwall.com/p/fw6miw/reverse-text-in-golang
+//https://golang.org/doc/effective_go.html#for
 
+
+//implements main method
 package main
 
+//import the format package
 import "fmt"
 
+//main method function
 func main() {
-    s := "Hello, playground"
-    fmt.Println(s)
-
-    fmt.Println(Reverse(s))
-    fmt.Println(ReverseUsingRunes(s))
-
+    s := "Hello, playground" //creates a slice(an array in go)  
+    fmt.Println(s) //displays a slice of string to the user 
+    fmt.Println(Reverse(s)) //reverses the slice using the Reverse method
 }
 
+//Reverse method function
 func Reverse(s string) string {
-    var reverse string
+    var reverse string //declares a string called reverse
     for i := len(s)-1; i >= 0; i-- {
-        reverse += string(s[i])
+        reverse += string(s[i])//reverses the characters in the string by using the for method 
     }
-    return reverse 
+    return reverse //returns the string
 }
 
-func ReverseUsingRunes(s string) string {
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
-}
