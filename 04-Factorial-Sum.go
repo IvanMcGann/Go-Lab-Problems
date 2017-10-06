0fd2aea86d397b1020f1b7128905f8e869f99538
Ivan McGann <g00340138@gmit.ie>
2017-10-06 14:57:46 +0100

    adding links where I found code to do factorial, really struggled with this program over the last few days.

--- 04-Factorial-Sum.go
+++ 04-Factorial-Sum.go
@@ -1,3 +1,6 @@
+//Adapted from : https://play.golang.org/p/yW7sAyJpPe
+//				 http://golang-jp.org/pkg/math/big/	
+// https://stackoverflow.com/questions/11270547/go-big-int-factorial-with-recursion	
 
 package main
 
