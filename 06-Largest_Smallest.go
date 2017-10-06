c795711efe137ec853c3336ac4552f82f30d5cd9
Ivan McGann <g00340138@gmit.ie>
2017-10-06 13:12:51 +0100

    placed code for problem to see which is the biggest and smallest number in an array

--- 06-Largest_Smallest.go
+++ 06-Largest_Smallest.go
@@ -1,9 +1,34 @@
+
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
+  var n, smallest, biggest int
+  x := []int{
+    13,98,46,23,
+    57,64,
+  }
 
+  for _,v:=range x {
+    if v>n {
+      fmt.Println(v,"is greater than",n)
+      n = v
+      biggest = n
+    } else {
+      fmt.Println(v,"is less than",n)
+    }
+  }
+
+  fmt.Println("The biggest number is: ", biggest)
+  for _,v:=range x {
+    if v>n {
+      fmt.Println(v,">",n)
+    } else {
+      fmt.Println(v,"<",n)
+      n = v
+      smallest = n
+    }
+  }
+  fmt.Println("The smallest number is: ", smallest)
 }
