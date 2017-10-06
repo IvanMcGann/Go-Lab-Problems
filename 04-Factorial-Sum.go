4f7dcc534f0223fe0ffca6d131087c351a2b007a
IVAN MC GANN - Student <G00340138@gmit.ie>
2017-10-06 18:54:41 +0100

    added more comments to the w

--- 04-Factorial-Sum.go
+++ 04-Factorial-Sum.go
@@ -2,12 +2,15 @@
 //Adapted from : https://play.golang.org/p/yW7sAyJpPe
 //				 http://golang-jp.org/pkg/math/big/	
 // https://stackoverflow.com/questions/11270547/go-big-int-factorial-with-recursion			
+
+//implements main method
 package main
 
-
+//import the format and math/big package
+//math/big is used for big integers
 import (
 	"fmt"
-	"math/big" //math/big used for big ints
+	"math/big" 
 )
 
 
@@ -24,6 +27,7 @@ func factorial(n int64) *big.Int { //get factorial of a number. can handle big n
 
 }
 
+//main method function
 func main() {
 	fmt.Println(factorial(100)) //print out the factorial of 100
 	sum := big.NewInt(0) //set up sum as a big int 
