9d28333a856d6f6a3312f8b03b4bb6b0b8d35c1e
IVAN MC GANN - Student <G00340138@gmit.ie>
2017-10-06 17:07:35 +0100

    adding code to the sqrt and testing the exe

--- 09-Newton_SQRT.go
+++ 09-Newton_SQRT.go
@@ -4,3 +4,48 @@ import (
     "fmt"
     "math"
 )
+
+func Approx(x float64, z float64) float64 {
+
+    return z - (((z * z) - x) / (2 * z))
+
+}
+
+func Sqrt(x float64) float64 {
+
+    previous := 0.0
+    delta := 1.0
+    z := x
+
+    for delta > 0.005 {
+
+        z = Approx(x, z)
+
+        if previous > 0.0 {
+            delta = previous - z
+        }
+
+        previous = z
+    }
+
+    return z
+
+}
+
+func main() {
+
+    for i := 1; i < 11; i++ {
+
+        value := float64(i)
+
+        fmt.Println("Calculating Sqrt for ", value)
+
+        real := math.Sqrt(value)
+        approx := Sqrt(value)
+
+        fmt.Println("Real: ", real)
+        fmt.Println("Approx: ", approx)
+
+    }
+
+}
