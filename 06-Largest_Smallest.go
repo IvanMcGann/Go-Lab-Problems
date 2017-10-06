ab873f8c19b0ae2542975c4066c777386b39a97a
Ivan McGann <g00340138@gmit.ie>
2017-10-06 22:53:27 +0100

    added comments to the smallest and biggest; problem 6

--- 06-Largest_Smallest.go
+++ 06-Largest_Smallest.go
@@ -1,28 +1,31 @@
 //adapted from https://gist.github.com/pyk/10519339
 
-
+//implements main method
 package main
 
+//import the format package
 import "fmt"
 
 func main() {
-  var n, smallest, biggest int
+  var n, smallest, biggest int //created vriables for n which is the number, the smallest and biggest numbers
   x := []int{
     13,98,46,23,
     57,64,
-  }
+  } //creates int slice 
 
   for _,v:=range x {
     if v>n {
-      fmt.Println(v,"is greater than",n)
-      n = v
-      biggest = n
+      fmt.Println(v,"is greater than",n)// compares the current n starting with the first value v
+      n = v                             // n is given the value of v if it is greater than the previous v 
+      biggest = n                       // it is concurrently compares to the next value taken in until the slice is read
     } else {
-      fmt.Println(v,"is less than",n)
+      fmt.Println(v,"is less than",n) // if it is not greater than a println will display that
     }
   }
 
-  fmt.Println("The biggest number is: ", biggest)
+  fmt.Println("The biggest number is: ", biggest) // biggest value is displayed to console 
+
+  //for loop to find smallest, runs the same code as above but displays the smallest value
   for _,v:=range x {
     if v>n {
       fmt.Println(v,"is greater than",n)
@@ -32,5 +35,5 @@ func main() {
       smallest = n
     }
   }
-  fmt.Println("The smallest number is: ", smallest)
+  fmt.Println("The smallest number is: ", smallest) // smallest value is displayed to the console
 }
