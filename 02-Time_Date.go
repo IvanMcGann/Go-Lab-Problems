262d7a61de6a08d6962db9456f19d2d6c6685b95
IVAN MC GANN - Student <G00340138@gmit.ie>
2017-10-06 18:44:37 +0100

    added comments for problem 2 displaying date and time on the console

--- 02-Time_Date.go
+++ 02-Time_Date.go
@@ -1,12 +1,15 @@
+//implements main method
 package main
 
+//import the format and time packages
 import (
 	"fmt"
 	"time"
 )
 
+//main method function
 func main() {
+	//displays the date and time to the user on the console
 	fmt.Println("The second problem: ")
-
 	fmt.Println("The date and time is ", time.Now())
 }
