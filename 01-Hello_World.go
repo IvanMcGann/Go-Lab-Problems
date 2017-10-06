0219e576f20809610bbb830bf1e637669e78bc4f
IVAN MC GANN - Student <G00340138@gmit.ie>
2017-10-06 18:40:35 +0100

    added comments for Hello World problem

--- 01-Hello_World.go
+++ 01-Hello_World.go
@@ -1,10 +1,14 @@
 // adapted from https://gobyexample.com/hello-world
 
+//implements main method
 package main
 
-import "fmt"
+//import format
+import "fmt" 
 
+//main method function
 func main() {
+  //using format displays hello world in English and Japanese
   fmt.Println("Hello World")
   fmt.Println("こんにちは世界")
 }
